bc9c5cf3eef07aef945dad1899ed17988d84091b
Kaitlyn Ly <kaitraq@gmail.com>
2018-09-09 21:34:17 -0700

    add lottie

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <lottie :options="defaultOptions" :height="500" :width="500" v-on:animCreated="handleAnimation"/>
+  </div>
+</template>
+
+<script>
+import lottie from '../components/lottie.vue'
+import * as animation from '../assets/coming_soon.json'
+
+export default {
+  name: 'HomePage',
+  components: {
+    'lottie': lottie
+  },
+  data () {
+    return {
+      defaultOptions: {animationData: animation},
+      animationSpeed: 0.4,
+    }
+  },
+  methods: {
+    handleAnimation: function (anim) {
+      this.anim = anim;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
