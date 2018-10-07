1691a7b8d09cfedb094b68c2024a4508ff7aa75f
Kaitlyn Ly <kaitlyn.ly@infusionsoft.com>
2018-10-07 13:21:54 -0700

    Add presentations and projects [page

---
+++ src/pages/ProjectsPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="content">
+        <h1>Projects</h1>
+        Coming soon!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectsPage',
+  components: {
+  },
+  data () {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@styles/main.scss';
+
+  .container {
+    display: flex;
+    flex-direction: row;
+    background-color: $color-lightestGray;
+  }
+
+  .content {
+    flex-grow: 1;
+    padding: $gp * 4;
+    color: $color-darkerGray;
+    overflow: auto;
+
+    @media only screen and (max-width: $small) {
+      padding: $gp;
+    }
+  }
+
+  h1 {
+    font-weight: 500;
+    border-bottom: 6px solid $color-lighterGray;
+  }
+</style>
